9d6c7ae28333fbe61c13f66ca6ce0a7ab8c6578a
The Caucajun <caucajun@gmail.com>
2016-03-05 12:27:49 -0600

    Initial commit of ShittyReddit files - Cx2

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+  session_start();
+  require"db_connect.php";
+  $sql = "SELECT page_id, page_title FROM pages;"
+
+?>
+
+<!doctypte html>
+<html>
+  
+  <head>
+    <meta charset="utf-8">
+      <title>
+      Welcome to Shitty Reddit!
+      </title>
+  </head>
+
+  <body>
+  </body>
+
+</html>
