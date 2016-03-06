3581e938e43a74f4d4c1938ef5f1104254d339ed
The Caucajun <caucajun@gmail.com>
2016-03-06 17:42:47 -0600

    Formatted all menus and layouts

--- new_user.php
+++ new_user.php
@@ -10,6 +10,7 @@
 	<title></title>
 </head>
 <body>
-   <?php   include("menu.php");  ?>
+    <?php   include("menu.php");  ?>
+ 	<p>This is my home page that uses a common menu to save me time when I add new pages to my website!</p>
 </body>
 </html>
--- user_login.php
+++ user_login.php
@@ -10,8 +10,7 @@
 	<title></title>
 </head>
 <body>
-  <br>
-   <?php   include("menu.php");  ?>
-  </br> 
+  <?php   include("menu.php");  ?>
+ 	<p>This is my home page that uses a common menu to save me time when I add new pages to my website!</p>
 </body>
 </html>
