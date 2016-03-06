6c73dee0e82e9e7c1b4420bdc4fbf0017d4ef99c
The Caucajun <caucajun@gmail.com>
2016-03-06 17:36:55 -0600

    Adding menu to all pages

--- new_user.php
+++ new_user.php
@@ -1,1 +1,15 @@
 // File for adding a new user to the database and setting a password for login credentials
+
+
+
+
+
+<<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+   <?php   include("menu.php");  ?>
+</body>
+</html>
--- user_login.php
+++ user_login.php
@@ -1,1 +1,15 @@
 // File for logging in an existing user in the database
+
+
+
+
+
+<<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+   <?php   include("menu.php");  ?>
+</body>
+</html>
