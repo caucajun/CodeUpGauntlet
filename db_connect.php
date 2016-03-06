6cb86dcbf90090a5cfd5089535e8ecad613bc2be
The Caucajun <caucajun@gmail.com>
2016-03-06 17:01:07 -0600

    HTML syntax check for br

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,10 @@
 <?php
   $link = mysql_connect('localhost', 'chris', 'Caillouet', 'redditapp');
+ 
   if (!$link) {
     die('Could not connect: ' . mysql_error());
-  }
-  echo 'Connected successfully to the ShittyReddit Database!                    Impressive.  Most impressive!  But you are not a Jedi, yet.';
+              }
+
+  echo '<br> Connected successfully to the ShittyReddit Database!  </br>   Impressive.  Most impressive!  But you are not a Jedi, yet.';
   mysql_close($link);
 ?>
