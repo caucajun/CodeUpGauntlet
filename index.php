056f4b71d216ddc2445916cdc232c29f5fc8fa53
The Caucajun <caucajun@gmail.com>
2016-03-06 17:32:19 -0600

    Adding Menu Titles

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
   require"db_connect.php";
   $sql = "SELECT page_id, page_title FROM pages;"
 
- // include("menu.php");
 
 ?>
 
@@ -18,7 +17,7 @@
   </head>
 
   <body>
-
+    <?php   include("menu.php");  ?>
  	<p>This is my home page that uses a common menu to save me time when I add new pages to my website!</p>
   </body>
 
