eceff559e6f028313bccbe0b1cdfa59953112476
The Caucajun <caucajun@gmail.com>
2016-03-06 18:03:13 -0600

    Adding frontpage

---
+++ page.php
@@ -0,0 +1,25 @@
+<?php
+
+// File for displaying full content of all pages (Linux, Apache, ...) [frontpage of Reddit] and posts (identified by page) [subreddits]
+
+?>
+
+
+
+<<!DOCTYPE html>
+<html>
+
+<head>
+
+  <title>
+	The Frontpage of ShittyReddit
+  </title>
+
+</head>
+
+  <body>
+    <?php   include("menu.php");  ?>
+    <p>Welcome to our Frontpage!</p>
+  </body>
+
+</html>
