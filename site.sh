c372caa8cc4ff5b1de460352819a35c1d8407f5e
root <root@server-01.(none)>
2016-03-06 19:24:47 -0600

    Adding site.sh - Server Side Automated Depolyment Script

--- site.sh
+++ site.sh
@@ -1,4 +1,4 @@
-# Server Site Automated Deployment Script
+## Server Site Automated Deployment Script
 rm -rf CodeUpGauntlet
 git clone https://github.com/caucajun/CodeUpGauntlet.git
 cp -af CodeUpGauntlet/* /var/www/html/
